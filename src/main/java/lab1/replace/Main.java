a7c0cb25e1666455b5b9b2dbbf31d63d5cc3d25d
Maria <mashakanyushkova@gmail.com>
2020-02-18 22:50:32 +0300

    вынесены функции по замене подстрок

--- src/main/java/lab1/replace/Main.java
+++ src/main/java/lab1/replace/Main.java
@@ -15,7 +15,7 @@ public class Main {
             String replace = args[3];
 
             String fileContent = FileManager.read(inputFile);
-            String replaced = Main.replaceString(fileContent, search, replace);
+            String replaced = StringReplacer.replaceString(fileContent, search, replace);
             FileManager.write(outputFile, replaced);
 
         } catch (Exception error) {
@@ -23,35 +23,4 @@ public class Main {
         }
     }
 
-    private static String replaceString(String fileContent, String searchStr, String replaceStr) {
-        StringBuilder outString = new StringBuilder();
-        boolean canReplace = searchStr.length() > 0;
-        if (!canReplace) {
-            return fileContent;
-        }
-
-        for (int i = 0; i < fileContent.length(); ) {
-            if (Main.findSubstring(fileContent, i, searchStr)) {
-                i += searchStr.length();
-                outString.append(replaceStr);
-            } else {
-                outString.append(fileContent.charAt(i));
-                ++i;
-            }
-        }
-        return outString.toString();
-    }
-
-    private static boolean findSubstring(String fileContent, Integer index, String substr) {
-        if ((fileContent.length() - index) >= substr.length() && substr.length() > 0) {
-            if (Main.checkFirstAndLastSymbols(fileContent.charAt(index), substr.charAt(0), fileContent.charAt(index + substr.length() - 1), substr.charAt(substr.length() - 1))) {
-                return fileContent.substring(index, index + substr.length()).equals(substr);
-            }
-        }
-        return false;
-    }
-
-    private static boolean checkFirstAndLastSymbols(char first1, char first2, char last1, char last2) {
-        return first1 == first2 && last1 == last2;
-    }
 }
