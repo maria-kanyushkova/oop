ca8eac0bb58925fb4c78ba212042a74aa1ea14a5
Maria <mashakanyushkova@gmail.com>
2020-02-28 19:04:24 +0300

    (1 lab invert): поправлены наименования функций

--- src/main/java/lab1/invert/Matrix.java
+++ src/main/java/lab1/invert/Matrix.java
@@ -1,22 +1,22 @@
 package lab1.invert;
 
 public class Matrix {
-    static double[][] inverse(double[][] matrix) {
+    static double[][] inverse(double[][] matrix) throws ArithmeticException {
         double determinant = getDeterminant(matrix);
         if (determinant == 0) {
             throw new ArithmeticException("No inverse matrix");
         }
-        return multiply(transpose(cofactor(matrix)), 1.0 / determinant);
+        return multiply(transpose(calculateAdditionalMatrix(matrix)), 1.0 / determinant);
     }
 
     static double[][] transpose(final double[][] matrix) {
-        double[][] transposed = new double[matrix.length][matrix.length];
+        double[][] transposedMatrix = new double[matrix.length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                transposed[j][i] = matrix[i][j];
+                transposedMatrix[j][i] = matrix[i][j];
             }
         }
-        return transposed;
+        return transposedMatrix;
     }
 
     static double[][] multiply(final double[][] matrix, double constant) {
@@ -35,26 +35,19 @@ public class Matrix {
         }
         if (matrix.length == 3) {
             return (
-                    matrix[0][0] * matrix[1][1] * matrix[2][2] +
-                    matrix[0][1] * matrix[1][2] * matrix[2][0] +
-                    matrix[0][2] * matrix[1][0] * matrix[2][1] -
-                    matrix[0][2] * matrix[1][1] * matrix[2][0] -
-                    matrix[0][1] * matrix[1][0] * matrix[2][2] -
-                    matrix[0][0] * matrix[1][2] * matrix[2][1]
+                matrix[0][0] * matrix[1][1] * matrix[2][2] +
+                matrix[0][1] * matrix[1][2] * matrix[2][0] +
+                matrix[0][2] * matrix[1][0] * matrix[2][1] -
+                matrix[0][2] * matrix[1][1] * matrix[2][0] -
+                matrix[0][1] * matrix[1][0] * matrix[2][2] -
+                matrix[0][0] * matrix[1][2] * matrix[2][1]
             );
         }
         return 0;
     }
 
-    private static int inverse(final int i) {
-        if (i % 2 == 0) {
-            return 1;
-        }
-        return -1;
-    }
-
-    static double[][] createSubMatrix(final double[][] matrix, int rows, int cols) {
-        double[][] mat = new double[matrix.length - 1][matrix.length - 1];
+    static double[][] createSubMatrix(final double[][] matrix, int rows, int columns) {
+        double[][] subMatrix = new double[matrix.length - 1][matrix.length - 1];
         int r = -1;
         for (int i = 0; i < matrix.length; i++) {
             if (i == rows) {
@@ -63,22 +56,23 @@ public class Matrix {
             r++;
             int c = -1;
             for (int j = 0; j < matrix.length; j++) {
-                if (j == cols) {
+                if (j == columns) {
                     continue;
                 }
-                mat[r][++c] = matrix[i][j];
+                subMatrix[r][++c] = matrix[i][j];
             }
         }
-        return mat;
+        return subMatrix;
     }
 
-    static double[][] cofactor(final double[][] matrix) {
-        double[][] mat = new double[matrix.length][matrix.length];
+    static double[][] calculateAdditionalMatrix(final double[][] matrix) {
+        double[][] additionalMatrix = new double[matrix.length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                mat[i][j] = inverse(i + j) * getDeterminant(createSubMatrix(matrix, i, j));
+                int sign = (i + j) % 2 == 0 ? 1 : -1;
+                additionalMatrix[i][j] = sign * getDeterminant(createSubMatrix(matrix, i, j));
             }
         }
-        return mat;
+        return additionalMatrix;
     }
 }
