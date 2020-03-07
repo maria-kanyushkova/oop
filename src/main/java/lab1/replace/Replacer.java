a035af5e34b316a5709f9a45dd9286e1449b4c4f
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-08 00:30:59 +0300

    issue#1: поправлены замечания по try-with-resources

--- src/main/java/lab1/replace/Replacer.java
+++ src/main/java/lab1/replace/Replacer.java
@@ -4,20 +4,19 @@ import java.io.*;
 
 public class Replacer {
     public static void replaceFile(File inputFile, File outputFile, String searchStr, String replaceStr) throws IOException {
-
-        FileReader fileReader = new FileReader(inputFile);
-        FileWriter writer = new FileWriter(outputFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String stringLine;
-        while ((stringLine = bufferedReader.readLine()) != null) {
-            String newString = stringLine;
-            if (searchStr.length() > 0) {
-                newString = stringLine.replace(searchStr, replaceStr);
+        try (
+                FileReader fileReader = new FileReader(inputFile);
+                FileWriter writer = new FileWriter(outputFile);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ) {
+            String stringLine;
+            while ((stringLine = bufferedReader.readLine()) != null) {
+                String newString = stringLine;
+                if (searchStr.length() > 0) {
+                    newString = stringLine.replace(searchStr, replaceStr);
+                }
+                writer.write(newString + "\n");
             }
-            writer.write(newString + "\n");
         }
-        bufferedReader.close();
-        fileReader.close();
-        writer.close();
     }
 }
