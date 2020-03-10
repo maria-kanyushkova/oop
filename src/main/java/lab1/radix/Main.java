0e72a962ce74236b5fead53a25289e4b749113f8
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-10 16:18:38 +0300

    (issue#2): добавлено DTO для формирования объекта вводимых данных

--- src/main/java/lab1/radix/Main.java
+++ src/main/java/lab1/radix/Main.java
@@ -1,20 +1,14 @@
 package lab1.radix;
 
-import common.ArgsValidator;
-
 public class Main {
     public static void main(String[] args) {
         try {
-            String expectedFormat = "<from> <to> <value>";
-            ArgsValidator.validate(args, 3, expectedFormat);
-            final int from = Integer.parseInt(args[0]);
-            final int to = Integer.parseInt(args[1]);
-            String value = args[2];
-
-            Helper.checkNumberSystemsValid(from, to);
-            Helper.checkValueValid(value, from);
-
-            System.out.println(Converter.convert(value, from, to));
+            RadixDTO radixDto = new RadixDTO(args);
+            System.out.println(Converter.convert(
+                    radixDto.getValue(),
+                    radixDto.getFromRadix(),
+                    radixDto.getToRadix()
+            ));
         } catch (Exception error) {
             System.out.println(error.getLocalizedMessage());
         }
---
+++ src/main/java/lab1/radix/RadixDTO.java
@@ -0,0 +1,50 @@
+package lab1.radix;
+
+import javax.management.StringValueExp;
+
+public class RadixDTO {
+    private static int fromRadix;
+    private static int toRadix;
+    private static String value;
+
+    public RadixDTO(String[] args) throws IllegalArgumentException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Few arguments.\nExpected format: <from> <to> <value>");
+        }
+        int fromRadix = Integer.parseInt(args[0]);
+        int toRadix = Integer.parseInt(args[1]);
+        String value = args[2];
+
+        Helper.checkNumberSystemsValid(fromRadix, toRadix);
+        Helper.checkValueValid(value, fromRadix);
+
+        setFromRadix(fromRadix);
+        setToRadix(toRadix);
+        setValue(value);
+
+    }
+
+    private static void setFromRadix(int from) {
+        fromRadix = from;
+    }
+
+    private static void setToRadix(int to) {
+        toRadix = to;
+    }
+
+    private static void setValue(String valueOfNumber) {
+        value = valueOfNumber;
+    }
+
+    public int getFromRadix() {
+        return fromRadix;
+    }
+
+    public int getToRadix() {
+        return toRadix;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
