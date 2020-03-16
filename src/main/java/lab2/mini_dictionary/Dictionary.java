d36d79eedf1548590bf7c766c78c49b3704b3d3f
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-16 10:14:05 +0300

    добавлены файлы lw2 task3 mini_dictionary

---
+++ src/main/java/lab2/mini_dictionary/Dictionary.java
@@ -0,0 +1,36 @@
+package lab2.mini_dictionary;
+
+import java.util.List;
+import java.util.Map;
+
+public class Dictionary {
+    private static Map<String, List<String>> dictionary;
+
+    Dictionary() {
+        // loadFileContent()
+        // цикл консольных запросов
+        // if word -> search (t -> addWordTranslate) (f->addWord and translate)
+        // if ... -> ожидание У или у
+    }
+
+    private static void loadFileContent() {
+
+    }
+
+    private static void saveFileContent() {
+
+    }
+
+    private static void searchWord() {
+      // searchWord.toLowerCase()
+    }
+
+    private static void addWord() {
+        // add translate word
+        // else add dictionary word (english)
+    }
+
+    private static void onExit() {
+
+    }
+}
