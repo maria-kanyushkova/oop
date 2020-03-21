8dade5371af5bdd67729a0be003ce7be3b3d6790
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-21 21:06:01 +0300

    подрефакторено lw1 fill

--- src/main/java/lab1/fill/Main.java
+++ src/main/java/lab1/fill/Main.java
@@ -2,16 +2,27 @@ package lab1.fill;
 
 import common.FileManager;
 
+import java.io.File;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            FillDTO fillDTO = new FillDTO(args);
-            char[][] aria = Utils.parse(fillDTO.getFileContent());
+            FillDTO fillDTO = parseArgs(args);
+            File inputFile = FileManager.getFileByPath(fillDTO.getInputPath());
+            File outputFile = FileManager.create(fillDTO.getOutputPath());
+            List<List<Character>> aria = Utils.parse(inputFile);
             aria = Filler.fill(aria);
-            FileManager.write(fillDTO.getOutputFile(), Utils.print(aria));
+            FileManager.write(outputFile, Utils.print(aria));
         } catch (Exception error) {
             System.out.println(error.getLocalizedMessage());
         }
     }
 
+    private static FillDTO parseArgs(String[] args) throws IllegalArgumentException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Few arguments.\nExpected format: <inputFile> <outputFile>");
+        }
+        return new FillDTO(args[0], args[1]);
+    }
 }
