8342d35447edfb699d146c64d989c99853934f55
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-24 20:44:30 +0300

    поправлены тесты для lw2 url_parser

--- src/test/java/lab2/url_parser/ParserTest.java
+++ src/test/java/lab2/url_parser/ParserTest.java
@@ -11,49 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
     @Test
-    public void parseThrowsIfInputIsIncorrect() {
-        final String input = "habr";
-        final Parser parser = new Parser(input);
-        assertThrows(MalformedURLException.class, parser::parse);
-    }
-
-    @Test
-    public void parseThrowsOnUnknownProtocol() {
-        final String input = "ws://unknown.protocol.com";
-        final Parser parser = new Parser(input);
-        assertThrows(IOException.class, parser::parse);
-    }
-
-    @Test
-    public void parseThrowsOnIncorrectUrl() {
-        final String input = "http://";
-        final Parser parser = new Parser(input);
-        assertThrows(URISyntaxException.class, parser::parse);
-    }
-
-    @Test
-    public void parseThrowsIfNoProtocol() {
-        final String input = "habr.com";
-        final Parser parser = new Parser(input);
-        assertThrows(IOException.class, parser::parse);
-    }
-
-    @Test
-    public void parseThrowsIfPortIsMore65535() {
-        final String input = "http://habr.com:70000";
-        final Parser parser = new Parser(input);
-        assertThrows(IOException.class, parser::parse);
-    }
-
-    @Test
-    public void parseThrowsIfPortIsNegative() {
-        final String input = "http://habr.com:-2/test.pdf";
-        final Parser parser = new Parser(input);
-        assertThrows(MalformedURLException.class, parser::parse);
-    }
-
-    @Test
-    public void parseWorksCorrectlyMediumSize() throws IOException, URISyntaxException {
+    public void shouldParseWorksCorrectlyMediumSize() throws IOException, URISyntaxException {
         final String input = "http://www.mysite.com/docs/document1.html?page=30&lang=en#title";
         final Parser parser = new Parser(input);
         final String expected = "http://www.mysite.com/docs/document1.html?page=30&lang=en#title\n" +
@@ -64,7 +22,7 @@ public class ParserTest {
     }
 
     @Test
-    public void parseWorksCorrectlyLongSize() throws IOException, URISyntaxException {
+    public void shouldParseWorksCorrectlyLongSize() throws IOException, URISyntaxException {
         final String input = "https://raw.githubusercontent.com/IntersectAustralia/acdata/master/sample_files/Panalytical.XRDML";
         final Parser parser = new Parser(input);
         final String expected = "https://raw.githubusercontent.com/IntersectAustralia/acdata/master/sample_files/Panalytical.XRDML\n" +
@@ -75,7 +33,7 @@ public class ParserTest {
     }
 
     @Test
-    public void parseWorksCorrectlyShortSize() throws IOException, URISyntaxException {
+    public void shouldParseWorksCorrectlyShortSize() throws IOException, URISyntaxException {
         final String input = "https://habr.com";
         final Parser parser = new Parser(input);
         final String expected = "https://habr.com\n" +
@@ -85,7 +43,7 @@ public class ParserTest {
     }
 
     @Test
-    public void parseWorksCorrectlyWithQueries() throws IOException, URISyntaxException {
+    public void shouldParseWorksCorrectlyWithQueries() throws IOException, URISyntaxException {
         final String input = "ftp://www.mysite.com/docs/document1.html?page=30&lang=en#title";
         final Parser parser = new Parser(input);
         final String expected = "ftp://www.mysite.com/docs/document1.html?page=30&lang=en#title\n" +
@@ -96,7 +54,7 @@ public class ParserTest {
     }
 
     @Test
-    public void parseWorksCorrectlyWithQueriesAndCustomPort() throws IOException, URISyntaxException {
+    public void shouldParseWorksCorrectlyWithQueriesAndCustomPort() throws IOException, URISyntaxException {
         final String input = "ftp://www.mysite.com:1337/docs/document1.html?page=30&lang=en#title";
         final Parser parser = new Parser(input);
         final String expected = "ftp://www.mysite.com:1337/docs/document1.html?page=30&lang=en#title\n" +
@@ -104,5 +62,47 @@ public class ParserTest {
                 "PORT: 1337\n" +
                 "DOC: docs/document1.html?page=30&lang=en#title";
         assertEquals(expected, parser.parse().print());
+    }
+
+    @Test
+    public void shouldParseThrowsIfInputIsIncorrect() {
+        final String input = "habr";
+        final Parser parser = new Parser(input);
+        assertThrows(MalformedURLException.class, parser::parse);
+    }
+
+    @Test
+    public void shouldParseThrowsOnUnknownProtocol() {
+        final String input = "ws://unknown.protocol.com";
+        final Parser parser = new Parser(input);
+        assertThrows(IOException.class, parser::parse);
+    }
+
+    @Test
+    public void shouldParseThrowsOnIncorrectUrl() {
+        final String input = "http://";
+        final Parser parser = new Parser(input);
+        assertThrows(URISyntaxException.class, parser::parse);
+    }
+
+    @Test
+    public void shouldParseThrowsIfNoProtocol() {
+        final String input = "habr.com";
+        final Parser parser = new Parser(input);
+        assertThrows(IOException.class, parser::parse);
+    }
+
+    @Test
+    public void shouldParseThrowsIfPortIsMore65535() {
+        final String input = "http://habr.com:70000";
+        final Parser parser = new Parser(input);
+        assertThrows(IOException.class, parser::parse);
+    }
+
+    @Test
+    public void shouldParseThrowsIfPortIsNegative() {
+        final String input = "http://habr.com:-2/test.pdf";
+        final Parser parser = new Parser(input);
+        assertThrows(MalformedURLException.class, parser::parse);
     }
 }
