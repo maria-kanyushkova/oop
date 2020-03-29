8b8e6f00f3b882d5a4e0864520114eb5aece96f5
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-29 17:57:49 +0300

    поправлен класс car в lw3 car

--- src/main/java/lab3/car/Car.java
+++ src/main/java/lab3/car/Car.java
@@ -1,76 +1,68 @@
 package lab3.car;
 
+import java.time.temporal.ValueRange;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Car {
-    private boolean statusOfCar = false;
+    private boolean isEnabled = false;
     private Directions direction = Directions.STAND;
     private int currentSpeed = 0;
-    private int currentGear = 0;
+    private Gear currentGear = Gear.NEUTRAL;
+    private Map<Gear, ValueRange> gearInfo = new HashMap<>();
+
+    Car() {
+        gearInfo.put(Gear.REVERSE, ValueRange.of(0, 20));
+        gearInfo.put(Gear.NEUTRAL, ValueRange.of(0, 150));
+        gearInfo.put(Gear.FIRST, ValueRange.of(0, 30));
+        gearInfo.put(Gear.SECOND, ValueRange.of(20, 50));
+        gearInfo.put(Gear.THIRD, ValueRange.of(30, 60));
+        gearInfo.put(Gear.FOURTH, ValueRange.of(40, 90));
+        gearInfo.put(Gear.FIFTH, ValueRange.of(50, 150));
+    }
 
     public boolean turnOnEngine() {
-        if (!statusOfCar) {
-            statusOfCar = true;
-        }
-        return statusOfCar;
-    }
-
-    public boolean turnOffEngine() {
-        if (!statusOfCar) {
-            return statusOfCar;
-        }
-        if (statusOfCar && direction == Directions.STAND && currentGear == 0) {
-            statusOfCar = false;
-        }
-        return statusOfCar;
-    }
-
-    public boolean setGear(int gear) {
-        Gear gearBuffer = getGearInfo(gear);
-        if (gearBuffer.inRange(currentSpeed)) {
-            currentGear = gear;
+        if (!isEnabled) {
+            isEnabled = true;
             return true;
         }
         return false;
     }
 
+    public boolean turnOffEngine() {
+        boolean isStandingStill = direction == Directions.STAND && currentGear == Gear.NEUTRAL && currentSpeed == 0;
+        if (!isEnabled || !isStandingStill) {
+            return false;
+        }
+        isEnabled = false;
+        return false;
+    }
+
+    public boolean setGear(Gear gear) {
+        if (!gearInfo.containsKey(gear) || gearInfo.get(gear).isValidValue(currentSpeed)) {
+            return false;
+        }
+        currentGear = gear;
+        return true;
+    }
+
     public boolean setSpeed(int speed) {
-        Gear gearBuffer = getGearInfo(currentGear);
-        if (gearBuffer.inRange(speed) || (currentSpeed >= speed && gearBuffer == Gear.NEUTRAL)) {
+        if (gearInfo.get(currentGear).isValidValue(speed) || (currentSpeed >= speed && currentGear == Gear.NEUTRAL)) {
             currentSpeed = speed;
             return true;
         }
         return false;
     }
 
-    private void changeDirection(int nextTransmission) {
+    private void changeDirection(int nextGear) {
         if (currentSpeed == 0) {
             direction = Directions.STAND;
         }
-        if ((currentGear > 0 || nextTransmission == 0) && currentSpeed > 0) {
+        if ((currentGear.toNumber() > 0 || nextGear == 0) && currentSpeed > 0) {
             direction = Directions.FORWARD;
         }
-        if ((currentGear < 0 || nextTransmission == 0) && currentSpeed > 0) {
+        if ((currentGear.toNumber() < 0 || nextGear == 0) && currentSpeed > 0) {
             direction = Directions.BACK;
-        }
-    }
-
-    private Gear getGearInfo(int gear) {
-        switch(gear) {
-            case -1:
-                return Gear.REVERSE_GEAR;
-            case 0:
-                return Gear.NEUTRAL;
-            case 1:
-                return Gear.FIRST_GEAR;
-            case 2:
-                return Gear.SECOND_GEAR;
-            case 3:
-                return Gear.THIRD_GEAR;
-            case 4:
-                return Gear.FOURTH_GEAR;
-            case 5:
-                return Gear.FIFTH_GEAR;
-            default:
-                return null;
         }
     }
 }
--- src/main/java/lab3/car/Gear.java
+++ src/main/java/lab3/car/Gear.java
@@ -1,37 +1,21 @@
 package lab3.car;
 
 enum Gear {
-    REVERSE_GEAR(-1, 0, 20),
-    NEUTRAL(0, 0, 150),
-    FIRST_GEAR(1, 0, 30),
-    SECOND_GEAR(2, 20, 50),
-    THIRD_GEAR(3, 30, 60),
-    FOURTH_GEAR(4, 40, 90),
-    FIFTH_GEAR(5, 50, 150);
+    REVERSE(-1),
+    NEUTRAL(0),
+    FIRST(1),
+    SECOND(2),
+    THIRD(3),
+    FOURTH(4),
+    FIFTH(5);
 
-    private final int speedName;
-    private final int minSpeed;
-    private final int maxSpeed;
-
-    Gear(int speedName, int minSpeed, int maxSpeed) {
-        this.speedName = speedName;
-        this.minSpeed = minSpeed;
-        this.maxSpeed = maxSpeed;
+    Gear(int gear) {
+        this.gear = gear;
     }
 
-    int getSpeedName() {
-        return speedName;
-    }
+    private final int gear;
 
-    int getMinSpeed() {
-        return minSpeed;
-    }
-
-    int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    boolean inRange(int speed) {
-        return minSpeed >= speed && speed >= maxSpeed;
+    public final int toNumber() {
+        return gear;
     }
 }
