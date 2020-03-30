6657c642ea6ccb26f870da06ef0fdab59150b566
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2020-03-30 16:45:51 +0300

    issue#7 добавлен тест с проверкой на пустой массив и поправленая функция обработки массива

--- src/main/java/lab2/vector/ProcessVector.java
+++ src/main/java/lab2/vector/ProcessVector.java
@@ -1,17 +1,18 @@
 package lab2.vector;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ProcessVector {
     public static List<Float> multiplyOnMaxAndMinElement(List<Float> numbers) {
-        List<Float> array = new ArrayList<>();
+        if (numbers.size() == 0) {
+            return numbers;
+        }
         final float maxNumber = Collections.max(numbers);
         final float minNumber = Collections.min(numbers);
-        for (Float number : numbers) {
-            array.add(number * maxNumber / minNumber);
+        for (int i = 0; i < numbers.size(); i++) {
+            numbers.set(i, numbers.get(i) * maxNumber / minNumber);
         }
-        return array;
+        return numbers;
     }
 }
