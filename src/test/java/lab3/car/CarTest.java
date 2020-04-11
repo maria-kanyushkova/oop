2b68725777a5749bd2bf0bd33eb6748afc6b11ff
Maria <mashakanyushkova@gmail.com>
2020-04-11 17:49:30 +0300

    lw3 car обновлена структура тестов для Car

--- src/test/java/lab3/car/CarTest.java
+++ src/test/java/lab3/car/CarTest.java
@@ -1,290 +1,256 @@
 package lab3.car;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CarTest {
+    public Car car;
 
-    // 01 - машина выключена проверка начальных состояний (скорость, направление, передача, выключенность)
+    @BeforeEach
+    public void init() {
+        car = new Car();
+    }
+
     @Test
-    public void shouldCorrectlyCarState() {
-        Car car = new Car();
+    @DisplayName("car initial state is correct")
+    public void shouldCorrectlyInitialState() {
         assertFalse(car.getEnable());
         assertEquals(car.getDirection(), Direction.STAND);
         assertEquals(car.getSpeed(), 0);
         assertEquals(car.getGear(), Gear.NEUTRAL);
     }
 
-    // 02 - машина выключена, нельзя изменить скорость и пердачу
-    @Test
-    public void shouldNotShiftValueForSpeedOrGearForOffVehicle() {
-        Car car = new Car();
-        assertFalse(car.getEnable());
-        assertFalse(car.setGear(Gear.FIRST));
-        assertFalse(car.setSpeed(12));
+    @Nested
+    @DisplayName("when car is turn off")
+    class CarIsTurnOff {
+        @Test
+        @DisplayName("shouldn't turn off car")
+        public void shouldNotTurnOffRepeatedly() {
+            assertFalse(car.turnOffEngine());
+        }
+
+        @Test
+        @DisplayName("should switch on neutral gear")
+        public void shouldSwitchOnNeutralGear() {
+            assertTrue(car.setGear(Gear.NEUTRAL));
+        }
+
+        @Test
+        @DisplayName("shouldn't change speed and gear")
+        public void shouldNotChangeSpeedAndGear() {
+            assertFalse(car.setGear(Gear.REVERSE));
+            assertFalse(car.setGear(Gear.FIRST));
+            assertFalse(car.setSpeed(12));
+        }
     }
 
-    // 03 - машину выключена, нельзя выключить повторно
     @Test
-    public void shouldNotTurnOnInVehicleOff() {
-        Car car = new Car();
-        assertFalse(car.getEnable());
-        assertFalse(car.turnOffEngine());
-    }
-
-    // 04 - машина включена, нельзя включить повторно
-    @Test
-    public void shouldNotTurnOnIfEnabled() {
-        Car car = new Car();
-        car.turnOnEngine();
-        assertTrue(car.getEnable());
-        assertFalse(car.turnOnEngine());
-    }
-
-    // 05 - можно включить выключенную машину
-    @Test
+    @DisplayName("should turn on car")
     public void shouldTurnOn() {
-        Car car = new Car();
-        assertFalse(car.getEnable());
         assertTrue(car.turnOnEngine());
+        assertTrue(car.getEnable());
     }
 
-    // 06 - можно выключить включённую машину
-    @Test
-    public void shouldTurnOff() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.turnOffEngine());
-    }
+    @Nested
+    @DisplayName("when car is turn on")
+    class WhenCarIsTurnOn {
+        @BeforeEach
+        public void init() {
+            car.turnOnEngine();
+        }
 
-    // 23 - машина выключена, можно поменять передачу только на нейтральную
-    @Test
-    public void shouldShiftToNeutralGearInVehicleOff() {
-        Car car = new Car();
-        assertFalse(car.getEnable());
-        assertTrue(car.setGear(Gear.NEUTRAL));
-    }
+        @Test
+        @DisplayName("shouldn't turn on when car is already on")
+        public void shouldNotTurnOnRepeatedly() {
+            assertFalse(car.turnOnEngine());
+        }
 
-    // машина включена
+        @Test
+        @DisplayName("should turn off car")
+        public void shouldTurnOff() {
+            assertTrue(car.turnOffEngine());
+        }
 
-    // 07 - стоим: смогли переключить на первую передачу и поехать (скорость, передача и направление движения)
-    @Test
-    public void shouldShiftToFirstGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(12));
+        @Nested
+        @DisplayName("when car is standing")
+        class WhenCarIsStanding {
+            @Test
+            @DisplayName("should switch on first gear")
+            public void shouldSwitchOnFirstGear() {
+                assertEquals(car.getDirection(), Direction.STAND);
+                assertTrue(car.setGear(Gear.FIRST));
+                assertTrue(car.setSpeed(30));
 
-        assertEquals(car.getGear(), Gear.FIRST);
-        assertEquals(car.getSpeed(), 12);
-        assertEquals(car.getDirection(), Direction.FORWARD);
-    }
+                assertEquals(car.getGear(), Gear.FIRST);
+                assertEquals(car.getSpeed(), 30);
+                assertEquals(car.getDirection(), Direction.FORWARD);
+            }
 
-    // 08 - стоим: смогли переключиться на задний ход (скорость, передача и направление движения)
-    @Test
-    public void shouldShiftToReverseGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertTrue(car.setGear(Gear.REVERSE));
-        assertTrue(car.setSpeed(12));
+            @Test
+            @DisplayName("should switch on reverse gear")
+            public void shouldSwitchOnReverseGear() {
+                assertEquals(car.getDirection(), Direction.STAND);
+                assertTrue(car.setGear(Gear.REVERSE));
+                assertTrue(car.setSpeed(12));
 
-        assertEquals(car.getGear(), Gear.REVERSE);
-        assertEquals(car.getSpeed(), 12);
-        assertEquals(car.getDirection(), Direction.BACK);
-    }
+                assertEquals(car.getGear(), Gear.REVERSE);
+                assertEquals(car.getSpeed(), 12);
+                assertEquals(car.getDirection(), Direction.BACK);
+            }
 
-    //  09 - стоим: не можем переключиться на 5ую предачу
-    @Test
-    public void shouldNotShiftToFiveGearInStandDirection() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertFalse(car.setGear(Gear.FIFTH));
-    }
+            @Test
+            @DisplayName("shouldn't switch with neutral gear on last gear")
+            public void shouldNotSwitchOnLastGear() {
+                assertEquals(car.getDirection(), Direction.STAND);
+                assertFalse(car.setGear(Gear.FIFTH));
+            }
+        }
 
-    //  10 - едем: переключение с 1ой на 5ую передачу не получается
-    @Test
-    public void shouldNotShiftGearToFifthFromFirstGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
+        @Nested
+        @DisplayName("when car is moving")
+        class WhenCarIsMoving {
+            @BeforeEach
+            public void setMoving() {
+                car.setGear(Gear.FIRST);
+                car.setSpeed(30);
+            }
 
-        assertEquals(car.getDirection(), Direction.FORWARD);
-        assertEquals(car.getGear(), Gear.FIRST);
-        assertEquals(car.getSpeed(), 30);
+            @Test
+            @DisplayName("shouldn't switch with first gear on last gear")
+            public void shouldNotSwitchOnLastGear() {
+                assertEquals(car.getDirection(), Direction.FORWARD);
+                assertEquals(car.getGear(), Gear.FIRST);
+                assertEquals(car.getSpeed(), 30);
 
-        assertFalse(car.setGear(Gear.FIFTH));
-    }
+                assertFalse(car.setGear(Gear.FIFTH));
+            }
 
-    //  11 - едем: переключение с 1ой на -1ую передачу нельзя
-    @Test
-    public void shouldNotShiftFromFirstToReverseGearInMove() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
+            @Test
+            @DisplayName("shouldn't switch with first gear on reverse gear")
+            public void shouldNotSwitchOnReverseGear() {
+                assertEquals(car.getDirection(), Direction.FORWARD);
+                assertFalse(car.setGear(Gear.REVERSE));
+            }
 
-        assertEquals(car.getDirection(), Direction.FORWARD);
+            @Test
+            @DisplayName("shouldn't turn off car")
+            public void shouldNotTurnOff() {
+                assertFalse(car.turnOffEngine());
+            }
+        }
 
-        assertFalse(car.setGear(Gear.REVERSE));
-    }
+        @Test
+        @DisplayName("should moving to back before car stops")
+        public void shouldMovingToBackBeforeCarStops() {
+            assertTrue(car.setGear(Gear.REVERSE));
 
-    //  12 - переключение скоростей с 0 на -1 на 0 и на 5 (граничная скорость или не в диапазоне)
-    @Test
-    public void checkShiftGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertEquals(car.getGear(), Gear.NEUTRAL);
+            assertTrue(car.setSpeed(20));
+            assertEquals(car.getDirection(), Direction.BACK);
 
-        assertTrue(car.setGear(Gear.REVERSE));
-        assertFalse(car.setSpeed(30));
-        assertFalse(car.setSpeed(-10));
-        assertTrue(car.setSpeed(20));
-        assertTrue(car.setGear(Gear.NEUTRAL));
-        assertFalse(car.setSpeed(30));
-        assertFalse(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(0));
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
-        assertTrue(car.setGear(Gear.SECOND));
-        assertFalse(car.setSpeed(10));
-        assertTrue(car.setSpeed(50));
-        assertTrue(car.setGear(Gear.THIRD));
-        assertTrue(car.setGear(Gear.NEUTRAL));
-        assertTrue(car.setGear(Gear.THIRD));
-    }
+            assertTrue(car.setGear(Gear.NEUTRAL));
+            assertEquals(car.getDirection(), Direction.BACK);
 
-    //  13 - когда движемся и передача нейтральная, то движение остается пока не станет "стоим" (вперёд\назад)
-    @Test
-    public void shouldDirectionUnchangedUntilSpeedNotEqualToZero() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertEquals(car.getDirection(), Direction.STAND);
-        assertTrue(car.setGear(Gear.REVERSE));
-        assertTrue(car.setSpeed(20));
-        assertEquals(car.getDirection(), Direction.BACK);
-        assertTrue(car.setGear(Gear.NEUTRAL));
-        assertEquals(car.getDirection(), Direction.BACK);
-        assertTrue(car.setSpeed(1));
-        assertEquals(car.getDirection(), Direction.BACK);
-        assertTrue(car.setSpeed(0));
-        assertEquals(car.getDirection(), Direction.STAND);
+            assertTrue(car.setSpeed(0));
+            assertEquals(car.getDirection(), Direction.STAND);
+        }
 
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(20));
-        assertEquals(car.getDirection(), Direction.FORWARD);
-        assertTrue(car.setGear(Gear.NEUTRAL));
-        assertEquals(car.getDirection(), Direction.FORWARD);
-        assertTrue(car.setSpeed(1));
-        assertEquals(car.getDirection(), Direction.FORWARD);
-        assertTrue(car.setSpeed(0));
-        assertEquals(car.getDirection(), Direction.STAND);
-    }
+        @Test
+        @DisplayName("should moving to forward before car stops")
+        public void shouldMovingToForwardBeforeCarStops() {
+            assertTrue(car.setGear(Gear.FIRST));
 
-    // 15 - нельзя в движении выключить машину
-    @Test
-    public void shouldNotTurnOffWhileVehicleIsMoving() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(20));
-        assertFalse(car.turnOffEngine());
-    }
+            assertTrue(car.setSpeed(20));
+            assertEquals(car.getDirection(), Direction.FORWARD);
 
-    // 16 - проверка граничащих скоростей для -1 передачи
-    @Test
-    public void checkBoundarySpeedForReverseGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.REVERSE));
-        assertTrue(car.setSpeed(20));
-        assertFalse(car.setSpeed(21));
-        assertFalse(car.setSpeed(-1));
-    }
+            assertTrue(car.setGear(Gear.NEUTRAL));
+            assertEquals(car.getDirection(), Direction.FORWARD);
 
-    // 17 - проверка граничащих скоростей для 0 передачи
-    @Test
-    public void checkBoundarySpeedForNeutralGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.NEUTRAL));
-        assertTrue(car.setSpeed(0));
-        assertFalse(car.setSpeed(10));
-    }
+            assertTrue(car.setSpeed(0));
+            assertEquals(car.getDirection(), Direction.STAND);
+        }
 
-    // 18 - проверка граничащих скоростей для 1 передачи
-    @Test
-    public void checkBoundarySpeedForFirstGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(15));
-        assertFalse(car.setSpeed(-1));
-        assertFalse(car.setSpeed(31));
-    }
+        @Test
+        @DisplayName("should correct boundary speed for reverse gear")
+        public void checkBoundarySpeedForReverseGear() {
+            assertTrue(car.setGear(Gear.REVERSE));
+            assertTrue(car.setSpeed(20));
+            assertFalse(car.setSpeed(21));
+            assertFalse(car.setSpeed(-1));
+        }
 
-    // 19 - проверка граничащих скоростей для 2 передачи
-    @Test
-    public void checkBoundarySpeedForSecondGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(20));
-        assertTrue(car.setGear(Gear.SECOND));
-        assertTrue(car.setSpeed(30));
-        assertFalse(car.setSpeed(19));
-        assertFalse(car.setSpeed(51));
-    }
+        @Test
+        @DisplayName("should correct boundary speed for neutral gear")
+        public void checkBoundarySpeedForNeutralGear() {
+            assertTrue(car.setGear(Gear.NEUTRAL));
+            assertTrue(car.setSpeed(0));
+            assertFalse(car.setSpeed(10));
+        }
 
-    // 20 - проверка граничащих скоростей для 3 передачи
-    @Test
-    public void checkBoundarySpeedForThirdGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
-        assertTrue(car.setGear(Gear.THIRD));
-        assertTrue(car.setSpeed(40));
-        assertFalse(car.setSpeed(29));
-        assertFalse(car.setSpeed(61));
-    }
+        @Test
+        @DisplayName("should correct boundary speed for first gear")
+        public void checkBoundarySpeedForFirstGear() { ;
+            assertTrue(car.setGear(Gear.FIRST));
+            assertTrue(car.setSpeed(15));
+            assertFalse(car.setSpeed(-1));
+            assertFalse(car.setSpeed(31));
+        }
 
-    // 21 - проверка граничащих скоростей для 4 передачи
-    @Test
-    public void checkBoundarySpeedForFourthGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
-        assertTrue(car.setGear(Gear.THIRD));
-        assertTrue(car.setSpeed(40));
-        assertTrue(car.setGear(Gear.FOURTH));
-        assertTrue(car.setSpeed(50));
-        assertFalse(car.setSpeed(39));
-        assertFalse(car.setSpeed(91));
-    }
+        @Test
+        @DisplayName("should correct boundary speed for second gear")
+        public void checkBoundarySpeedForSecondGear() {
+            car.setGear(Gear.FIRST);
+            car.setSpeed(20);
 
-    // 22 - проверка граничащих скоростей для 5 передачи
-    @Test
-    public void checkBoundarySpeedForFifthGear() {
-        Car car = new Car();
-        assertTrue(car.turnOnEngine());
-        assertTrue(car.setGear(Gear.FIRST));
-        assertTrue(car.setSpeed(30));
-        assertTrue(car.setGear(Gear.THIRD));
-        assertTrue(car.setSpeed(50));
-        assertTrue(car.setGear(Gear.FIFTH));
-        assertTrue(car.setSpeed(100));
-        assertFalse(car.setSpeed(49));
-        assertFalse(car.setSpeed(151));
+            assertTrue(car.setGear(Gear.SECOND));
+            assertTrue(car.setSpeed(30));
+            assertFalse(car.setSpeed(19));
+            assertFalse(car.setSpeed(51));
+        }
+
+        @Test
+        @DisplayName("should correct boundary speed for third gear")
+        public void checkBoundarySpeedForThirdGear() {
+            car.setGear(Gear.FIRST);
+            car.setSpeed(30);
+
+            assertTrue(car.setGear(Gear.THIRD));
+            assertTrue(car.setSpeed(40));
+            assertFalse(car.setSpeed(29));
+            assertFalse(car.setSpeed(61));
+        }
+
+        @Test
+        @DisplayName("should correct boundary speed for fourth gear")
+        public void checkBoundarySpeedForFourthGear() {
+            car.setGear(Gear.FIRST);
+            car.setSpeed(30);
+            car.setGear(Gear.THIRD);
+            car.setSpeed(40);
+
+            assertTrue(car.setGear(Gear.FOURTH));
+            assertTrue(car.setSpeed(50));
+            assertFalse(car.setSpeed(39));
+            assertFalse(car.setSpeed(91));
+        }
+
+        @Test
+        @DisplayName("should correct boundary speed for fifth gear")
+        public void checkBoundarySpeedForFifthGear() {
+            car.setGear(Gear.FIRST);
+            car.setSpeed(30);
+            car.setGear(Gear.THIRD);
+            car.setSpeed(50);
+
+            assertTrue(car.setGear(Gear.FIFTH));
+            assertTrue(car.setSpeed(100));
+            assertFalse(car.setSpeed(49));
+            assertFalse(car.setSpeed(151));
+        }
     }
 }
