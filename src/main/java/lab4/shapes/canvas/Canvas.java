ab8d4378785686e94b6d0581524da4337593db5a
Maria <mashakanyushkova@gmail.com>
2020-05-07 21:00:26 +0300

    lw4 shapes поправлено замечание по Canvas

--- src/main/java/lab4/shapes/canvas/Canvas.java
+++ src/main/java/lab4/shapes/canvas/Canvas.java
@@ -2,69 +2,43 @@ package lab4.shapes.canvas;
 
 import lab4.shapes.common.Point;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
-public class Canvas extends JPanel implements ICanvas {
-    private static final int STROKE_WIDTH = 2;
-    private List<Function<Graphics2D, Void>> painter = new ArrayList<>();
+public class Canvas implements ICanvas {
+    private Graphics graphics = new Graphics();
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        initGraphics2D(g2d);
-
-        painter.forEach(p -> p.apply(g2d));
+    public Graphics getGraphics() {
+        return graphics;
     }
 
     @Override
     public void drawLine(Point from, Point to, Color outlineColor) {
-        painter.add((Graphics2D g2d) -> {
-            g2d.setPaint(outlineColor);
-            g2d.draw(new Line2D.Double(from.getX(), from.getY(), to.getX(), to.getY()));
-            return null;
-        });
+        graphics.setPaint(outlineColor);
+        graphics.draw(new Line2D.Double(from.getX(), from.getY(), to.getX(), to.getY()));
     }
 
     @Override
     public void fillPolygon(List<Point> points, Color fillColor) {
-        painter.add((Graphics2D g2d) -> {
-            g2d.setColor(fillColor);
-            Path2D path = new Path2D.Double();
-            path.moveTo(points.get(0).getX(), points.get(0).getY());
-            points.stream().skip(1).forEach(point -> path.lineTo(point.getX(), point.getY()));
-            path.closePath();
-            g2d.fill(path);
-            return null;
-        });
+        Path2D path = new Path2D.Double();
+        path.moveTo(points.get(0).getX(), points.get(0).getY());
+        points.stream().skip(1).forEach(point -> path.lineTo(point.getX(), point.getY()));
+        path.closePath();
+
+        graphics.setColor(fillColor);
+        graphics.fill(path);
     }
 
     @Override
     public void drawCircle(Point center, double radius, Color outlineColor) {
-        painter.add((Graphics2D g2d) -> {
-            g2d.setColor(outlineColor);
-            g2d.draw(new Ellipse2D.Double(center.getX(), center.getY(), radius, radius));
-            return null;
-        });
+        graphics.setPaint(outlineColor);
+        graphics.draw(new Ellipse2D.Double(center.getX(), center.getY(), radius, radius));
     }
 
     @Override
     public void fillCircle(Point center, double radius, Color fillColor) {
-        painter.add((Graphics2D g2d) -> {
-            g2d.setColor(fillColor);
-            g2d.fill(new Ellipse2D.Double(center.getX(), center.getY(), radius, radius));
-            return null;
-        });
-    }
-
-    private void initGraphics2D(final Graphics2D g2d) {
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setStroke(new BasicStroke(STROKE_WIDTH));
+        graphics.setPaint(fillColor);
+        graphics.fill(new Ellipse2D.Double(center.getX(), center.getY(), radius, radius));
     }
 }
---
+++ src/main/java/lab4/shapes/canvas/Graphics.java
@@ -0,0 +1,55 @@
+package lab4.shapes.canvas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Graphics extends JPanel {
+    private static final int STROKE_WIDTH = 2;
+    private List<Function<Graphics2D, Void>> painter = new ArrayList<>();
+
+    public void setColor(Color color) {
+        painter.add((Graphics2D g2d) -> {
+            g2d.setColor(color);
+            return null;
+        });
+    }
+
+    public void draw(Shape shape) {
+        painter.add((Graphics2D g2d) -> {
+            g2d.draw(shape);
+            return null;
+        });
+    }
+
+    public void fill(Shape shape) {
+        painter.add((Graphics2D g2d) -> {
+            g2d.fill(shape);
+            return null;
+        });
+    }
+
+    public void setPaint(Color color) {
+        painter.add((Graphics2D g2d) -> {
+            g2d.setPaint(color);
+            return null;
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        initGraphics2D(g2d);
+
+        painter.forEach(p -> p.apply(g2d));
+    }
+
+    private void initGraphics2D(final Graphics2D g2d) {
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setStroke(new BasicStroke(STROKE_WIDTH));
+    }
+}
